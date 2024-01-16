d7bf457b27883ad5e3a6739fe1eba3af62b4115f
Calin Neamtu <nilac8991@gmail.com>
2024-01-16 16:00:03 +0100

    Add CommandIdentifiers to use when listening for results coming from DW
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

---
+++ datawedgeconfigurator/src/main/java/com/zebra/jamesswinton/datawedgewrapperlib/models/CommandIdentifier.kt
@@ -0,0 +1,11 @@
+package com.zebra.jamesswinton.datawedgewrapperlib.models
+
+enum class CommandIdentifier {
+    CREATE_PROFILE,
+    SET_CONFIG,
+    DW_SERIAL_COMMAND,
+    INTENT_API,
+    ADF_API,
+    KEYSTROKE_API,
+    SET_INTENT_OUTPUT;
+}
